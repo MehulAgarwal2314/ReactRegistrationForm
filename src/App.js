501049e874f6c2bac82b4466f6041f344a5855b0
agarwal <contact.mehul2314@gmail.com>
2023-05-07 13:07:23 +0530

    Add existing file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+function Title(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-items">
+      <p className="title">Registration Form</p>
     </div>
   );
 }
 
-export default App;
+// function RegisteredMessage(isShown,setIsShown,handleClick){
+//   return(
+//     <>
+//     {isShown && (
+//       <div>
+//         <h2>Some content here</h2>
+//       </div>
+//     )}
+//     </>
+//   );
+// }
+
+function Placeholders({Name,Email,Password,onNameChange,onEmailChange,onPasswordChange}) {
+  return (
+    <>
+      <form>
+        <div class="form-group">
+        <div class="col-sm-11">
+      <input type="text" value={Name} onChange={(e) => onNameChange(e.target.value)} class="form-control" placeholder="Enter your name"/>
+    </div>
+    </div>
+        <div class="form-group">
+          <div class="col-sm-11">
+          <input type="email" value={Email} onChange={(e) => onEmailChange(e.target.value)} class="form-control" placeholder="Enter your email"/>
+        </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-11">
+          <input type="password" value={Password} onChange={(e) => onPasswordChange(e.target.value)} class="form-control" placeholder="Enter Password"/>
+        </div>
+        </div>
+      </form>
+    </>
+  );
+}
+
+function Button({handleClick}) {
+  return (
+    <div className="button">
+      <button onClick={handleClick}>Register</button>
+    </div>
+  );
+}
+
+function PlaceholdersButton() {
+  const [Name, setName] = useState('');
+  const [Email, setEmail] = useState('');
+  const [Password, setPassword] = useState('');
+  const [isShown, setIsShown] = useState(false);
+
+  function isValidEmail(Email) {
+    return /\S+@\S+\.\S+/.test(Email);
+  }
+  
+  const handleClick = event => {
+    event.preventDefault();
+    
+    
+    if (!isValidEmail(Email)) {
+      alert('The Email is Invalid');
+    } 
+    
+    if (Name.trim().length !== 0 && Email.trim().length !== 0 && Password.trim().length !== 0 && isValidEmail(Email)  ) {    
+      console.log("Hi you have registered successfully !");
+      setIsShown(true);
+      return false;
+
+    }
+
+    else if(Name.trim().length === 0 || Email.trim().length === 0 ||  Password.trim().length === 0 ) {
+      alert("All fields are mandatory");
+      return false;
+    }
+  };
+
+  return (
+    <>
+     {isShown && (
+        <div className="naya">
+          <h3>Hello {Name}, You Have Registered Successfully !</h3>
+        </div>
+      )}
+      <div className="PlaceholdersButton">
+       
+        <Placeholders Name={Name} Email={Email} Password={Password}
+        onNameChange={setName} onEmailChange={setEmail} onPasswordChange={setPassword}/>
+        <Button Name={Name} Email={Email} Password={Password} handleClick={handleClick} />
+      </div>
+    </>
+  );
+}
+
+function RegistrationForm() {
+  return (
+    <div className="container">
+      <Title />
+      <PlaceholdersButton />
+    </div>
+  );
+}
+
+export default function App() {
+  return <RegistrationForm />;
+}
